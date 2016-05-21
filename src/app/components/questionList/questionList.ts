f3d5e6c29f262921ab75d4dccbcfc75d00007a08
michaelbahr <mbahr92@gmail.com>
2016-05-21 21:06:53 +0200

    finished the question side

--- src/app/components/questionList/questionList.ts
+++ src/app/components/questionList/questionList.ts
@@ -2,13 +2,15 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 import {ChatFormComponent} from '../chatForm/chatForm';
 import {MessageListComponent} from '../messageList/messageList';
 import {APP_SERVICES} from '../../services/services';
+import { UnicodeToDatePipe } from '../../unicodetodate.pipe';
 
 
 @Component({
     selector: 'questionList',
     templateUrl: 'app/components/questionList/questionList.html',
     directives: [ChatFormComponent, MessageListComponent],
-    providers: APP_SERVICES
+    providers: APP_SERVICES,
+    pipes: [UnicodeToDatePipe]
 })
 export class QuestionListComponent {
     @Input()questionList:Array;
